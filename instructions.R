c088385bbe32ba1a1730ffb1d95206959c1a1466
Michael Mott <michaelwarrenmott@gmail.com>
2013-11-20 15:27:06 -0500

    Beginning to learn how to use git. First push.

---
+++ instructions.R
@@ -0,0 +1,11 @@
+#Load govdat
+library(govdat)
+#Load dataset senate
+senate = read.csv("http://compute-cuj.org/data2/day1/nyt_senate.csv", as.is=TRUE)
+#loop
+for(i in 1:nrow(senate)){
+  tmp = sll_cw_phrases("legislator",senate$id[i],key="b1884b38a7e04ffeb9bdc91a0a63fe1b")
+  print(paste(senate$last_name[i], tmp$ngram[1]))
+}
+
+sll_cw_phrases("legislator","B001261",key="b1884b38a7e04ffeb9bdc91a0a63fe1b")
