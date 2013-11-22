d06c4555ff832385c28bc4d9b1ccb7de2545cd70
Michael Mott <michaelwarrenmott@gmail.com>
2013-11-22 12:37:47 -0500

    Added plots and timeseries for top drone users.

--- mikework.R
+++ mikework.R
@@ -17,6 +17,13 @@ for(i in 1:nrow(senate)){
   print(paste(senate$last_name[i], tmp$ngram[1]))
 }
 
+#drones and drone plots since '96
+dronets= sll_cw_timeseries("drone","1996-01-01","2013-11-22", key="b1884b38a7e04ffeb9bdc91a0a63fe1b")
+#134 records returned
+dronests= sll_cw_timeseries("drones","1996-01-01","2013-11-22", key="b1884b38a7e04ffeb9bdc91a0a63fe1b")
+#169 records returned
+plot(dronests$day,dronests$count)
+plot(dronets$day,dronets$count)
 
 #Creates ggplot of word 'drone' over time sorted by party.
 install.packages(ggplot2)
@@ -28,3 +35,14 @@ dat_r$party <- rep("R", nrow(dat_r))
 dat_both <- rbind(dat_d, dat_r)
 ggplot(dat_both, aes(day, count, colour=party)) + geom_line() +
   theme_grey(base_size=20) + scale_colour_manual(values=c("blue","red"))
+
+#Show me top legislators who use the word drone - from here we might be able 
+#to see whether they stopped? Also, not sure how to have drones + drone. This is drone.
+sll_cw_entity_phrases(entity="legislator",phrase='drone',sort="count", key="b1884b38a7e04ffeb9bdc91a0a63fe1b")
+#Drones
+sll_cw_entity_phrases(entity="legislator",phrase='drones',sort="count", key="b1884b38a7e04ffeb9bdc91a0a63fe1b")
+
+# Top legislator is Rand Paul, bioguide - P000603. The only time he said
+# the word drone was on March 6th, where he said it 235 times in a filibuster
+# against Obama, whom he claimed was advocating for a drone strike program
+# within the U.S.
