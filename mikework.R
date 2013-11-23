fdc195a7fe424f40dfc6be2b859043434a123b13
Michael Mott <michaelwarrenmott@gmail.com>
2013-11-22 19:26:46 -0500

    Reorganized, added intro, cleaned up. Still needs Alex's bit.

--- mikework.R
+++ mikework.R
@@ -1,35 +1,26 @@
-# Capitolwords group project - Mike's work
+# Capitolwords group project - Mike, Alex, Brian
 
-# Original loop code from class
-install.packages(devtools)
-library(devtools)
-install_github("govdat","cocteau")
-library(govdat)
-for(i in 1:nrow(senate)){
-  tmp = sll_cw_phrases("legistlator", senate$id[i], 
-                       key = "6a0998f8ba3c4a8b888ef28b20975174")
-  print(paste(senate$last_name[i], tmp$ngram[1]))
-}
+# In our second attempt of tackling the CapitolWords API project, 
+# we worked through timeseries' of the top users of the word "drone." 
+# We found 20 top users, did an example timeseries for one, and also
+# did a ggplot of all the users over time and a timeseries for all 
+# occurences of "drone" for context.
 
+# We then worked to figure out how to do it as a loop for all 20 of them. 
+# Currently we are stuck on that loop, the error message for which is below.
 
-# Attempt at making timeseries loop. Emailed mark.
-for(i in 1:nrow(senate)){
-  tmp = sll_cw_timeseries("drones", "1996-01-01", "2013-11-22", senate$id[i], 
-                          key = "6a0998f8ba3c4a8b888ef28b20975174")
-  print(paste(senate$last_name[i], tmp$ngram[1]))
-}
+# Mark helped us create dotchart also to better see our data, which shows
+# the word drone said over time sorted by the last time speakers said it.
+# Our goal from here on is to look at that chart, find interesting outliers
+# and figure out if there are connections between those who spoke the most 
+# about drones, those who sponsor bills, and those who are being paid in
+# campaign contributions by top drone manufacturers.
 
-# drones and drone plots since '96
-dronets= sll_cw_timeseries("drone","1996-01-01","2013-11-22", 
-                           key="b1884b38a7e04ffeb9bdc91a0a63fe1b")
-# 134 records returned
-dronests= sll_cw_timeseries("drones","1996-01-01","2013-11-22", 
-                            key="b1884b38a7e04ffeb9bdc91a0a63fe1b")
-# 169 records returned
-plot(dronests$day,dronests$count)
+# "Drone" word in congressional record since '96
+dronets= sll_cw_timeseries("drone",key="b1884b38a7e04ffeb9bdc91a0a63fe1b")
 plot(dronets$day,dronets$count)
 
-# Creates ggplot of word 'drone' over time sorted by party.
+# ggplot of word 'drone' over time, sorted by party.
 install.packages(ggplot2)
 library(ggplot2)
 dat_d <- sll_cw_timeseries(phrase='drones', party="D", 
@@ -42,26 +33,35 @@ dat_both <- rbind(dat_d, dat_r)
 ggplot(dat_both, aes(day, count, colour=party)) + geom_line() +
   theme_grey(base_size=20) + scale_colour_manual(values=c("blue","red"))
 
-# Show me top legislators who use the word drone - from here we might be able 
-# to see whether they stopped? Also, not sure how to have drones + drone. This 
-# is drone.
-topdroneusers=sll_cw_entity_phrases(entity="legislator",phrase='drone',sort="count", 
-                      key="b1884b38a7e04ffeb9bdc91a0a63fe1b")
-# Drones
-topdronesusers=sll_cw_entity_phrases(entity="legislator",phrase='drones',sort="count", 
-                      key="b1884b38a7e04ffeb9bdc91a0a63fe1b")
+# Original loop code from class
+install.packages(devtools)
+library(devtools)
+install_github("govdat","cocteau")
+library(govdat)
+for(i in 1:nrow(senate)){
+  tmp = sll_cw_phrases("legislator", senate$id[i], 
+                       key = "6a0998f8ba3c4a8b888ef28b20975174")
+  print(paste(senate$last_name[i], tmp$ngram[1]))
+}
 
-# Top legislator is Rand Paul, bioguide - P000603. The only time he said
-# the word drone was on March 6th, where he said it 235 times in a filibuster
-# against Obama, whom he claimed was advocating for a drone strike program
-# within the U.S.
-sll_cw_timeseries("drone", bioguide_id="P000603", "1996-01-01","2013-11-22", 
-                  key="b1884b38a7e04ffeb9bdc91a0a63fe1b")
+# Attempt at making timeseries loop. To be added to from Alex.
+for(i in 1:nrow(senate)){
+  tmp = sll_cw_timeseries("drones", senate$id[i], 
+                          key = "6a0998f8ba3c4a8b888ef28b20975174")
+  print(paste(senate$last_name[i], tmp$ngram[1]))
+}
 
-# Would like to see from month to month, who's usage of drone/s dropped 
-# below 50% of previous value? Or plot all of these?
+# Show top legislators who use the word drone - from here we might be able 
+# to see whether they stopped. Later we used the dotchart to better visualize this. 
+topdroneusers=sll_cw_entity_phrases(entity="legislator",phrase='drone',sort="count",key="b1884b38a7e04ffeb9bdc91a0a63fe1b")
 
-# Giving top 20 users of word drones w/ numbers
+# We noticed the top legislator to use "drone is Rand Paul, bioguide - P000603. 
+# The only time he said the word drone was on March 6th, where he said it 235 
+# times in a filibuster against Obama, whom he claimed was advocating for a drone 
+# strike program within the U.S.
+sll_cw_timeseries("drone", bioguide_id="P000603", "1996-01-01","2013-11-22", key="b1884b38a7e04ffeb9bdc91a0a63fe1b")
+
+# Top 20 users of word drones w/ numbers
 print(paste(topdroneusers$legislator[i],topdroneusers$count[1:20]))
 
 # Top 20 drone users - just the bioguides
@@ -73,17 +73,7 @@ for(i in topdroneusers$legislator[1:20]){
   plot(ts$day,ts$count)
 }
 
-# Cool. Going to use lattice/trellis/qq to do this.
-
-# Example plot (https://www.stat.auckland.ac.nz/~ihaka/787/lectures-trellis.pdf)
-Depth = equal.count(quakes$depth,number = 8,overlap = .1)
-xyplot(lat ~ long | Depth, data = quakes,xlab = "Longtitude", ylab = "Lattitude")
-
-# Trying to work through example plot w/ our data. Not sure where to go from here?
-Drones=equal.count(ts$count, number = 20, overlap =.1)
-xyplot(ts$count~ts$day | Drones, data=i)
-
-# Experimenting with working with drone legislation using OpenStates API:
+# Also experimented for future use with working with drone legislation using OpenStates API:
 
 #Installing relevant packages/getting JSON
 install.packages("rjson")
